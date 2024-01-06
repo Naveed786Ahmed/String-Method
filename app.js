162f76a3a3513accca956fe2312a639241a890f9
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2024-01-06 12:06:01 +0500

    String Method

---
+++ app.js
@@ -0,0 +1,199 @@
+// Question # 01
+
+// let firstName = prompt("Enter Your First Name")
+// let lastName = prompt("Enter Your Last Name")
+// let fullName = firstName + " " + lastName;
+
+// alert(`Asalam O Alikum ${fullName}`)
+
+// Question # 02
+
+// let phone = prompt("Enter Your Favorite Cell Phone Name");
+// let length = phone.length;
+
+// document.write(`My favorite phone is: ${phone} length of string: ${length}`)
+
+// Question # 03
+
+// let str = "Pakistani"
+// let index = str.indexOf("n")
+
+// document.write(`String: ${str} <br>`)
+// document.write(`Index of 'n': ${index}`)
+
+// Question # 04
+
+// let str = "Hello World"
+// let index = str.lastIndexOf("l")
+
+// document.write(`String: ${str} <br>`)
+// document.write(`Last index of 'l': ${index}`)
+
+// Question # 05
+
+// let str = "Pakistani"
+// let char = str.charAt(3)
+
+// document.write(`String: ${str} <br>`)
+// document.write(`Character at index 3: ${char}`)
+
+// Question # 06
+
+// let firstName = prompt("Enter Your First Name")
+// let lastName = prompt("Enter Your Last Name")
+// let fullName = firstName.concat(lastName);
+
+// alert(`Asalam O Alikum ${fullName}`)
+
+// Question # 07
+
+// let str = "Hyderabad"
+// let replace = str.replace("Hyder", "Islam")
+
+// document.write(`City: ${str} <br>`)
+// document.write(`After replacement: ${replace}`)
+
+// Question # 08
+
+// let msg = "Ali and Sami are best friends. They play cricket and football together."
+// let replace = msg.replaceAll("and", "&")
+
+// document.write(`City: ${msg} <br>`)
+// document.write(`After replacement: ${replace}`)
+
+// Question # 09
+
+// let str = "472"
+// let num = Number(str)
+
+// document.write(`Value: ${str} <br>`)
+// document.write(`Type: ${typeof str} <br>`)
+// document.write(`Value: ${str} <br>`)
+// document.write(`Type: ${typeof num}`)
+
+// Question # 10
+
+// let input = prompt(`Enter Your Word`)
+// let capital = input.toUpperCase()
+
+// document.write(`User input ${input} <br>`)
+// document.write(`User case ${capital}`)
+
+// Question # 11
+
+// let input = prompt(`Enter Your Word`)
+// let first = input.slice(0,1);
+// let after = input.slice(1)
+
+// first = first.toUpperCase()
+
+// document.write(`User input ${input} <br>`)
+// document.write(`Title case: ${first + after}`)
+
+// Question # 12
+
+// let num = 35.36;
+// num = num.toString();
+
+// let first = num.slice(0,2)
+// let last = num.slice(3)
+
+// document.write(`Number: ${num} <br>`)
+// document.write(`Result: ${first + last}`)
+
+// Question # 13
+
+// let userName = prompt(`Enter the Username`)
+
+// for(let i = 0; i < userName.length; i++){
+//     if(userName.charCodeAt(i) == 33 || userName.charCodeAt(i) == 44 || userName.charCodeAt(i) == 46 || userName.charCodeAt(i) == 64){
+//         userName = prompt(`Please enter a valid username`)
+//     }
+// }
+
+// Question # 14
+
+// let A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let search = prompt("Welcome to ABC Bakery. What do you want to order sir / ma'am?");
+// search = search.toLocaleLowerCase();
+// let searching = A.indexOf(search);
+// let msg = `We are sorry. ${search} is not available in our bakery`
+
+// for(let i = 0; i < A.length; i++){
+//     if(i == searching){
+//         msg = `${search} is available at index ${i} in our bakery`
+//     }
+// }
+
+// document.write(msg);
+
+// Question # 16
+
+// let university = "University of karachi"
+// let arr = university.split(" ")
+// console.log(arr)
+
+// for(let i = 0; i < university.length; i++){
+//     console.log(university[i])
+// }
+
+// Question # 17
+
+// let input = prompt("Enter a Value");
+// let lastChar = input.charAt(input.length - 1)
+
+// document.write(`User input: ${input} <br>`)
+// document.write(`Last character of input: ${lastChar}`)
+
+// Question # 18
+
+// let str = "The quick brown fox jumps over the lazy dog"
+// let arr = str.toLocaleLowerCase().split(" ");
+// let occurrences = "the";
+// let count = 0
+
+// for(let i = 0; i < str.length; i++){
+//     if(arr[i] == occurrences){
+//         count++;
+//     }
+// }
+
+// document.write(`Text: ${str} <br>`)
+// document.write(`There are ${count} occurrence(s) of word "${occurrences}"`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
